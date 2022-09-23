b7c2a7d727790d91b3ce9c51907804648f18f4e8
Ankit Jangid <ajangid663@gmail.com>
2022-09-23 19:44:04 +0530

    async operations

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,17 +8,47 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  void getData() async {
+    //simulate a network request
+    String user = await Future.delayed(Duration(seconds: 3), () {
+      return 'Virat';
+    });
+
+    //simulate network request to get bio of the username
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return 'lgbt,vegan, environmentalist, leftist, marxist';
+    });
+
+    print('$user is $bio');
+  }
+
+  int counter = 0;
+  @override
+  void initState() {
+    super.initState();
+    getData();
+
+    print('initState function ran');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build widget function ran of choose_location');
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: Text('Choose a location'),
-        centerTitle: true,
-        elevation: 0.0,
-      ),
-      body: Text('Choose location screen'),
-    );
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: const Text('Choose a location'),
+          centerTitle: true,
+          elevation: 0.0,
+        ),
+        body: ElevatedButton(
+          onPressed: () {
+            setState(() {
+              counter++;
+            });
+          },
+          child: Text('Counter is : $counter'),
+        ));
   }
 }
