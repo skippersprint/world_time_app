6f1346192bb7ff02e3c6363a637b40246b08b376
Ankit Jangid <ajangid663@gmail.com>
2022-09-23 18:56:58 +0530

    routing enabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/pages/choose_location.dart';
 import 'package:world_time_app/pages/home.dart';
+import 'package:world_time_app/pages/loading.dart';
+import 'package:world_time_app/pages/loading.dart';
+import 'package:world_time_app/pages/choose_location.dart';
 
-
-void main() => runApp(const MaterialApp(
-  home: Home(),
-));
+void main() => runApp(MaterialApp(
+  initialRoute: '/home',
+      routes: {
+        '/': (context) => Loading(),
+        '/home': (context) => Home(),
+        '/location': (context) => ChooseLocation()
+      },
+    ));
