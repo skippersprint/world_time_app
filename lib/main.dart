ea5d5754e18bb35d3b81f9cc5b076d283915b70b
Ankit Jangid <ajangid663@gmail.com>
2022-09-23 18:39:50 +0530

    wta first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,7 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
+import 'package:world_time_app/pages/home.dart';
+
 
 void main() => runApp(const MaterialApp(
-      home: QuoteList(),
-    ));
-
-class QuoteList extends StatefulWidget {
-  const QuoteList({super.key});
-
-  @override
-  State<QuoteList> createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote(author: 'Casey Niestat', text: 'DO MORE'),
-    Quote(text: 'Only the individual transcends', author: 'Naval'),
-    Quote(author: 'Rancho', text: 'All is well')
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: const Color.fromARGB(255, 187, 187, 187),
-        appBar: AppBar(
-          title: const Text('Motivational Quotes 🚀'),
-          centerTitle: true,
-          backgroundColor: const Color.fromARGB(255, 48, 48, 48),
-        ),
-        body: Center(
-          child: Column(
-            children: quotes
-                .map((x) => QuoteCard(
-                    e: x,
-                    delete: () {
-                      setState(() {
-                        quotes.remove(x);
-                      });
-                    }))
-                .toList(),
-          ),
-        ));
-  }
-}
+  home: Home(),
+));
--- lib/quote.dart
+++
@@ -1,7 +0,0 @@
-class Quote {
-  late String text;
-  late String author;
-  Quote({required this.text, required this.author});
-}
-
-//Quote myQuote = Quote(author: 'ankit', text: 'abc');
